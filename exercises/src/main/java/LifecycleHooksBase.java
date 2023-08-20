bac6aeb440874b54501c0bc46803740572cad0c7
amwtke <amwtke@gmail.com>
2023-08-20 13:56:10 +0800

    c4

--- exercises/src/main/java/LifecycleHooksBase.java
+++ exercises/src/main/java/LifecycleHooksBase.java
@@ -10,7 +10,10 @@ public class LifecycleHooksBase {
 
     public Flux<Integer> room_temperature_service() {
         return Flux.interval(Duration.ofMillis(100), Duration.ofMillis(100))
-                   .take(20)
-                   .map(i -> ThreadLocalRandom.current().nextInt(10, 30));
+                .take(20)
+                .map(i -> {
+                    System.out.println("thread:" + Thread.currentThread().getName() + " i:" + i);
+                    return ThreadLocalRandom.current().nextInt(10, 30);
+                });
     }
 }
